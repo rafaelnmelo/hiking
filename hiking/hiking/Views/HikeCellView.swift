6ac5b64517db9815786867511926c0380c2090f2
Rafael Nunes <rafaelnunesmelo@gmail.com>
2025-01-06 12:37:23 -0300

    Separando HikeCellView

---
+++ hiking/hiking/Views/HikeCellView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct HikeCellView: View {
+    
+    let hike: Hike
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Image(hike.photo)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                .frame(width: 100)
+            
+            VStack(alignment: .leading) {
+                Text(hike.name)
+                Text("\(hike.miles.formatted()) miles")
+            }
+        }
+    }
+}
--- hiking/hiking/Views/ListView.swift
+++ hiking/hiking/Views/ListView.swift
@@ -33,22 +33,3 @@ struct ListView: View {
     ListView()
 }
 
-struct HikeCellView: View {
-    
-    let hike: Hike
-    
-    var body: some View {
-        HStack(alignment: .top) {
-            Image(hike.photo)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
-                .frame(width: 100)
-            
-            VStack(alignment: .leading) {
-                Text(hike.name)
-                Text("\(hike.miles.formatted()) miles")
-            }
-        }
-    }
-}
